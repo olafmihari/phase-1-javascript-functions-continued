fcac30b00bf52af6fe6bb029429f1e888544cd26
Olaf Mihari <olafnjenga@gmail.com>
2023-03-14 18:20:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function mondayWork(e){
+    if (e==undefined){
+        e = 'go to the office';
+    }
+    return (`This Monday, I will ${e}.`)
+}
+
+function saturdayFun(e){
+    if (e==undefined){
+        e = 'roller-skate';
+    }
+    return (`This Saturday, I want to ${e}!`)
+}
+
+function wrapAdjective(e){
+    return function wrap(txt){
+        return `You are ${e}${txt}${e}!`
+    }
+
+}
